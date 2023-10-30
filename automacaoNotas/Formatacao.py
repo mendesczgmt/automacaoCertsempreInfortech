453ac3c459931104145368d9c8f0841ffe7411d4
Gabryel Mendes <gabryelmendecz@gmail.com>
2023-10-30 08:52:28 -0300

    Ajustando tarefas

--- automacaoNotas/Formatacao.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-class Formatacao:
-    def __init__(self):
-        self._data = datetime.date.today().strftime('%d/%m/%Y')
-    
-    def get_data(self):
-        return self._data
