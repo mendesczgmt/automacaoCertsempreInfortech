a3f7b6c443342f8ffa7e365bf6dfa43eb6595cdb
Gabryel Mendes <gabryelmendecz@gmail.com>
2023-10-31 19:52:59 -0300

    Faltando validar

--- automacaoBoletos/Main.py
+++ automacaoBoletos/Main.py
@@ -7,7 +7,7 @@ from Login import Login
 browser = webdriver.Chrome()
 browser.get("https://www.safe2pay.com.br/")
 browser.maximize_window()
-protocolo = "1004846398"
+protocolo = "1004859750"
 
 login = Login()
 
@@ -24,17 +24,47 @@ browser.find_element(By.XPATH, '/html/body/div[4]/div/div[3]/div/div/div/div/div
 time.sleep(5)
 
 def liberarBoleto():
-
-    browser.find_element(By.XPATH, '//*[@id="floattable"]/tbody/tr[1]/td[12]/small[1]').click()
-    browser.find_element(By.XPATH, '//*[@id="SidebarTransacao"]/div/div/div[2]/div[3]/div[1]/button').click()
-    browser.find_element(By.XPATH, '/html/body/div[13]/div/div[4]/div[2]/button').click()
-
-
-def buscarBoleto():
-    browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[2]/div/div[2]/div/button[3]').click()
-    time.sleep(1)
     try:
-        browser.find_element(By.ID, 'floattable').click()
+        browser.find_element(By.XPATH, '//*[@id="SidebarTransacao"]/div/div/div[2]/div[3]/div[1]/button').click()
+        time.sleep(20)
+        browser.find_element(By.XPATH, '/html/body/div[13]/div/div[4]/div[2]/button').click()
+    except:
+        print("erro no momento da liberação")
+
+if(len(protocolo) == 10):
+        browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[2]/div/div[1]/div/button').click()
+        time.sleep(1)
+        browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[2]/div/div[1]/div/ul/li[4]/label/input').click()
+        time.sleep(1)
+        browser.find_element(By.ID, 'txtFiltroCompradorReferencia').send_keys(protocolo)
+        browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[2]/div/div[2]/div/button[3]').click()
+        try:
+            browser.find_element(By.XPATH, '//*[@id="floattable"]/tbody/tr[1]').click()
+            time.sleep(1) 
+            liberarBoleto()
+        except:
+            browser.find_element(By.ID, 'btnLimparFiltros').click()
+            time.sleep(1)
+            browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[1]/form/div/div[11]/div/p/span/button').click()
+            time.sleep(1)
+            browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[1]/form/div/div[11]/div/p/div/ul/li[2]/div/div[1]/button').click()
+            browser.find_element(By.ID, 'txtFiltroCompradorReferencia').send_keys(protocolo)
+            browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[2]/div/div[2]/div/button[3]').click()
+            try:
+                time.sleep(1)
+                browser.find_element(By.XPATH, '//*[@id="floattable"]/tbody/tr[1]').click()
+                time.sleep(1)
+                liberarBoleto()
+            except:
+                print("erro")
+else:
+    time.sleep(1)
+    browser.find_element(By.ID, 'txtFiltroCodigo').send_keys(protocolo)
+    browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[2]/div/div[2]/div/button[3]').click()
+    try:
+        time.sleep(1)
+        browser.find_element(By.XPATH, '//*[@id="floattable"]/tbody/tr[1]').click()
+        time.sleep(1)
         liberarBoleto()
     except:
         time.sleep(1)
@@ -43,31 +73,16 @@ def buscarBoleto():
         browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[1]/form/div/div[11]/div/p/span/button').click()
         time.sleep(1)
         browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[1]/form/div/div[11]/div/p/div/ul/li[2]/div/div[1]/button').click()
-        time.sleep(1)
-        if(len(protocolo) == 10):
-            browser.find_element(By.ID, 'txtFiltroCompradorReferencia').send_keys(protocolo)
-        else:
-            browser.find_element(By.ID, 'txtFiltroCodigo').send_keys(protocolo)
-            browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[2]/div/div[2]/div/button[3]').click()
+        browser.find_element(By.ID, 'txtFiltroCodigo').send_keys(protocolo)
+        browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[2]/div/div[2]/div/button[3]').click()
+        try:
+            time.sleep(5)
+            browser.find_element(By.XPATH, '//*[@id="floattable"]/tbody/tr[1]').click()
+            time.sleep(2)
+            liberarBoleto()
+        except:
+            print("erro")
     
-        
-
-
-
-if(len(protocolo) == 10):
-    try:
-        browser.find_element(By.ID, 'txtFiltroCompradorReferencia').send_keys(protocolo)
-        buscarBoleto()
-    except:
-        browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[2]/div/div[1]/div/button').click()
-        time.sleep(1)
-        browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[1]/div[2]/div/div[1]/div/ul/li[4]/label/input').click()
-        time.sleep(1)
-        browser.find_element(By.ID, 'txtFiltroCompradorReferencia').send_keys(protocolo)
-        buscarBoleto()
-else:
-    browser.find_element(By.ID, 'txtFiltroCodigo').send_keys(protocolo)
-    buscarBoleto()
 
 
 
