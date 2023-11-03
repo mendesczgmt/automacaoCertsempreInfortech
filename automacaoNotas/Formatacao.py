e929198939dcd726f5d2a19c338d12923927a36d
Gabryel Mendes <gabryelmendecz@gmail.com>
2023-11-03 17:51:15 -0300

    faltando valor

--- automacaoNotas/Formatacao.py
+++ automacaoNotas/Formatacao.py
@@ -2,12 +2,22 @@ import datetime
 
 import pandas as pd
 
-lerPlanilha = pd.read_excel('C:\\Users\\Suporte\\Downloads\\PlanilhaSara.xlsx', sheet_name='16 a 20.10')
+shetname = '16 a 20.10'
+caminhoNotas = 'C:\\Users\\gabry\\Downloads\\Emissões.xlsx'
+lerPlanilha = pd.read_excel(caminhoNotas, sheet_name=shetname)
 
 class Formatacao:
     def __init__(self):
         self._data = datetime.date.today().strftime('%d/%m/%Y')
+        self._caminhoNotas = 'C:\\Users\\gabry\\Downloads\\Emissões.xlsx'
+        self._valor = 0
     
+    def formatarValor(self, valor):
+        self._valor = str(valor)
+        self._valor += "00"
+        self._valor = int(self._valor)
+        return self._valor
+
     def get_data(self):
         return self._data
 
@@ -15,6 +25,14 @@ class Formatacao:
         tamanho = lerPlanilha.shape
         num_linhas = tamanho[0]
         return num_linhas
+    
+    def get_caminhoNotas(self):
+        return caminhoNotas
 
+    def get_pegarValor(self):
+        self._valor = str(self._valor)
+        self._valor += "00"
+        self._valor = int(self._valor)
+        return self._valor
 
     
--- automacaoNotas/main.py
+++ automacaoNotas/main.py
@@ -8,19 +8,31 @@ from selenium.webdriver.common.keys import Keys
 from Login import Login
 from Formatacao import Formatacao
 
+shetname = '16 a 20.10'
+caminhoNotas = 'C:\\Users\\gabry\\Downloads\\Emissões.xlsx'
+
 browser = webdriver.Chrome()
-lerPlanilha = pd.read_excel('C:\\Users\\Suporte\\Downloads\\Emissões Sara Outubro (2).xlsx', sheet_name='16 a 20.10')
+browser.maximize_window()
+
+lerPlanilha = pd.read_excel(caminhoNotas, sheet_name=shetname)
 lerPlanilha['Protocolo'] = lerPlanilha['Protocolo'].astype(str)
 lerPlanilha['Documento'] = lerPlanilha['Documento'].astype(str)
 lerPlanilha['Valor do Boleto'] = lerPlanilha['Valor do Boleto'].astype(str)
 
 browser.get("https://sispmjp.joaopessoa.pb.gov.br:8080/nfse/login.jsf")
+browser.execute_script("window.open('', '_blank');")
+browser.switch_to.window(browser.window_handles[-1])
+browser.get("https://acsafeweb.safewebpss.com.br/gerenciamentoac")
+
+browser.switch_to.window(browser.window_handles[0])
+
 loginCpf = browser.find_element(By.ID, "j_username")
 password = browser.find_element(By.ID, "j_password")
 botaoEntrar = browser.find_element(By.ID, "commandButton_entrar")
 
 login = Login()
 formatacao = Formatacao()
+
 
 cpf = str(login.get_cpf())
 senha = str(login.get_senha())
@@ -38,48 +50,29 @@ for x in range(7):
 
 browser.get("https://sispmjp.joaopessoa.pb.gov.br:8080/nfse/paginas/nfse/NFSe_EmitirNFse.jsf")
 
-browser.execute_script("window.open('', '_blank');")
-browser.switch_to.window(browser.window_handles[-1])
-browser.maximize_window()
-browser.get("https://acsafeweb.safewebpss.com.br/gerenciamentoac")
-
 for x in range(int(formatacao.quantidadeNotas())):
+
     while True:
         try:
-            relatorio = browser.find_element(By.XPATH, '//*[@id="link6"]').click()
+            botaoNao = browser.find_element(By.XPATH, '//*[@id="form_emitir_nfse:selectradio_retencao_iss"]/tbody/tr/td[3]/div').click()
             break
         except:
             continue
 
-    relatorioEmissao = browser.find_element(By.XPATH, '//*[@id="link9"]').click()
-
-    campoProtocolo = browser.find_element(By.XPATH, '/html/body/app-root/div/div/app-pages/div[1]/div/div/ng-component/app-relatorio-emissao-lista/form/div/div/div[1]/div[2]/pages-filter/div/div[4]/div[1]/input')
-
     while True:
         try:
-            campoProtocolo.send_keys(lerPlanilha['Protocolo'][0])
+            formulario = browser.find_element(By.ID, 'form_emitir_nfse:calendar_competencia_input').send_keys(str(formatacao.get_data()))
             break
         except:
             continue
 
-    botaoPesquisarProtocolo = browser.find_element(By.XPATH, '/html/body/app-root/div/div/app-pages/div[1]/div/div/ng-component/app-relatorio-emissao-lista/form/div/div/div[1]/div[2]/pages-filter/div/div[9]/div/div/div/button[1]').click()
-
     while True:
         try:
-            lupaProtocolo = browser.find_element(By.XPATH, '/html/body/app-root/div/div/app-pages/div[1]/div/div/ng-component/app-relatorio-emissao-lista/form/div/div/div[2]/table/tbody/tr/td[2]/i[1]').click()
+            botaoProsseguir = browser.find_element(By.ID, 'form_emitir_nfse:commandButton_continuar').click()
             break
         except:
             continue
-
-    browser.switch_to.window(browser.window_handles[0])
-
-    botaoNao = browser.find_element(By.XPATH, '//*[@id="form_emitir_nfse:selectradio_retencao_iss"]/tbody/tr/td[3]/div')
-    botaoNao.click()
-    formulario = browser.find_element(By.ID, 'form_emitir_nfse:calendar_competencia_input')
-    formulario.send_keys(str(formatacao.get_data()))
-    botaoProsseguir = browser.find_element(By.ID, 'form_emitir_nfse:commandButton_continuar')
-    botaoProsseguir.click()
-
+    
     time.sleep(2)
 
     caixa_cpf_cnpj = browser.find_element(By.NAME, 'form_emitir_nfse:inputmask_cpf_cnpj')
@@ -89,33 +82,149 @@ for x in range(int(formatacao.quantidadeNotas())):
     botao_cpf_cnpj = browser.find_element(By.XPATH, '//*[@id="form_emitir_nfse:commandbutton_buscar_cpfcnpj"]/span').click()
     time.sleep(2)
 
-    botaoContinuar = browser.find_element(By.ID, 'form_emitir_nfse:commandButton_continuar').click()
-    time.sleep(2)
+    while True:
+        try:
+            browser.find_element(By.XPATH, '//*[@id="j_idt47:msgPrincipal"]/div') ## Spam sem cadastro
+            browser.switch_to.window(browser.window_handles[-1])
+            
+            while True:
+                try:
+                    relatorio = browser.find_element(By.XPATH, '//*[@id="link6"]').click()
+                    break
+                except:
+                    continue
+            
+            while True:
+                try:
+                    relatorioEmissao = browser.find_element(By.XPATH, '//*[@id="link9"]').click()  
+                    break
+                except:
+                    continue
+            
+            campoProtocolo = browser.find_element(By.XPATH, '/html/body/app-root/div/div/app-pages/div[1]/div/div/ng-component/app-relatorio-emissao-lista/form/div/div/div[1]/div[2]/pages-filter/div/div[4]/div[1]/input')
+            
+            while True:
+                try:
+                    campoProtocolo.send_keys(lerPlanilha['Protocolo'][x])
+                    break
+                except:
+                    continue
 
-    itemListaServico = browser.find_element(By.ID, 'form_emitir_nfse:selectOneMenu_lista_servico_label').click()
+            botaoPesquisarProtocolo = browser.find_element(By.XPATH, '/html/body/app-root/div/div/app-pages/div[1]/div/div/ng-component/app-relatorio-emissao-lista/form/div/div/div[1]/div[2]/pages-filter/div/div[9]/div/div/div/button[1]').click()
 
-    time.sleep(2)
+            while True:
+                try:
+                    lupaProtocolo = browser.find_element(By.XPATH, '/html/body/app-root/div/div/app-pages/div[1]/div/div/ng-component/app-relatorio-emissao-lista/form/div/div/div[2]/table/tbody/tr/td[2]/i[1]').click()
+                    break
+                except:
+                    continue
+       
+            while True:
+                try:
+                    razaoSocial = browser.find_element(By.XPATH, '/html/body/app-root/div/div/app-pages/div[1]/div/div/ng-component/app-relatorio-emissao-gestao/form/div/div/div[1]/div[2]/div[3]/div[1]/input').get_attribute("value")
+                    if(razaoSocial != ""):
+                        break
+                except:
+                    continue
 
-    servico = browser.find_element(By.ID, 'form_emitir_nfse:selectOneMenu_lista_servico_panel').click()
+            while True:
+                try:
+                    cep = browser.find_element(By.XPATH, '/html/body/app-root/div/div/app-pages/div[1]/div/div/ng-component/app-relatorio-emissao-gestao/form/div/div/div[1]/div[2]/div[6]/div[1]/input').get_attribute("value")
+                    if(cep != ""):
+                        break
+                except:
+                    continue
 
-    time.sleep(2)
+            while True:
+                try:
+                    logradouro = browser.find_element(By.XPATH, '/html/body/app-root/div/div/app-pages/div[1]/div/div/ng-component/app-relatorio-emissao-gestao/form/div/div/div[1]/div[2]/div[5]/div[1]/input').get_attribute("value")
+                    if(logradouro != ""):
+                        break
+                except:
+                    continue
+            
+            while True:
+                try:
+                    bairro = browser.find_element(By.XPATH, '/html/body/app-root/div/div/app-pages/div[1]/div/div/ng-component/app-relatorio-emissao-gestao/form/div/div/div[1]/div[2]/div[5]/div[3]/input').get_attribute("value")
+                    if(bairro != ""):
+                        break
+                except:
+                    continue
 
-    descricaoDetalhada = browser.find_element(By.ID, 'form_emitir_nfse:intputtextarea_descricao_detalhada')
-    descricaoDetalhada.click()
-    descricaoDetalhada.send_keys("Referente a emissão do Certificado Digital")
+            while True:
+                try:
+                    numero = browser.find_element(By.XPATH, '/html/body/app-root/div/div/app-pages/div[1]/div/div/ng-component/app-relatorio-emissao-gestao/form/div/div/div[1]/div[2]/div[5]/div[2]/input').get_attribute("value")
+                    if(numero != ""):
+                        break
+                except:
+                    continue
+            
+            browser.switch_to.window(browser.window_handles[0])
+            while True:
+                try:
+                    inputRazao = browser.find_element(By.ID, 'form_emitir_nfse:inputtext_nomeempresarial_nome').send_keys(razaoSocial)
+                    break
+                except:
+                    continue
+            inputCep = browser.find_element(By.ID, 'form_emitir_nfse:sispmjp_endereco:inputmask_cep').send_keys(cep)
+            botaoPesquisarCep = browser.find_element(By.XPATH, '//*[@id="form_emitir_nfse:sispmjp_endereco:commandButton_buscar"]/span').click()
+            inputLogradouro = browser.find_element(By.ID, 'form_emitir_nfse:sispmjp_endereco:inputtext_lagradouro').send_keys(logradouro)
+            inputBairro = browser.find_element(By.ID, 'form_emitir_nfse:sispmjp_endereco:inputtext_bairro').send_keys(bairro)
+            time.sleep(5)
+            inputNumero = browser.find_element(By.ID, 'form_emitir_nfse:sispmjp_endereco:inputmask_numero').send_keys(numero)
+            break
+        except:
+            break
 
-    time.sleep(2)
+    while True:
+        try:
+            botaoContinuar = browser.find_element(By.ID, 'form_emitir_nfse:commandButton_continuar').click()
+            break
+        except:
+            print("erro ao clicar")
+            continue
 
-    botaoContinuar = browser.find_element(By.ID, 'form_emitir_nfse:commandButton_continuar_mensagem').click()
-    time.sleep(2)
+    while True:
+        try:
+            itemListaServico = browser.find_element(By.ID, 'form_emitir_nfse:selectOneMenu_lista_servico_label').click()
+            break
+        except:
+            continue
 
-    botaoContinuar = browser.find_element(By.ID, 'form_emitir_nfse:commandButton_continuar').click()
-    time.sleep(2)
+    while True:
+        try:
+            servico = browser.find_element(By.ID, 'form_emitir_nfse:selectOneMenu_lista_servico_panel').click()
+            break
+        except:
+            continue
 
-    valorServico = browser.find_element(By.ID, 'form_emitir_nfse:intputmask_valor_servico')
-    valorServico.click()
+    while True:
+        try:
+            descricaoDetalhada = browser.find_element(By.ID, 'form_emitir_nfse:intputtextarea_descricao_detalhada').send_keys("Referente a emissão do Certificado Digital")
+            break
+        except:
+            continue
 
-    valorServico.send_keys(lerPlanilha['Valor do Boleto'][x])
+    while True:
+        try:
+            botaoContinuar = browser.find_element(By.ID, 'form_emitir_nfse:commandButton_continuar_mensagem').click()
+            break
+        except:
+            continue
+    while True:
+        try:
+            botaoContinuar = browser.find_element(By.ID, 'form_emitir_nfse:commandButton_continuar').click()
+            break
+        except:
+            continue
+    while True:
+        try:
+            valor = valorServico.send_keys(lerPlanilha['Valor do Boleto'][x])
+            valorServico = browser.find_element(By.ID, 'form_emitir_nfse:intputmask_valor_servico').send_keys(valor)
+            print("olá")
+            break
+        except:
+            print("holá")
+            continue
 
-
-
+time.sleep(50)
